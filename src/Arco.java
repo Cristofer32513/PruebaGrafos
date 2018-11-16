3aa316363d0b3073126265013add1179f825954d
Cristofer Casas <cristofer32513@gmail.com>
2018-11-16 15:09:40 -0600

    Se agrego la clase Arco.

---
+++ src/Arco.java
@@ -0,0 +1,24 @@
+class Arco{
+	//Atributos
+	int destino;
+    double peso;
+    
+    //Constructores
+    public Arco(int d){
+    	destino = d;
+    }
+    public Arco(int d, double p){
+    	this(d);
+    	peso = p;
+    }
+    
+    //Metodos
+    public int getDestino(){
+        return destino;
+    }
+   
+    public boolean equals(Object n){
+    	Arco a = (Arco)n;
+    	return destino == a.destino;
+    }
+}
